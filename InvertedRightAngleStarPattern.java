d4ac38cb9ecea0fa0613860a6ff25bebf70a2ec7
Rishab Ganguly <rishabhsunny25@gmail.com>
2026-07-22 01:26:39 +0530

    Day 1: Added Right Angle Star Pattern and Inverted Right Angle Star Pattern

---
+++ InvertedRightAngleStarPattern.java
@@ -0,0 +1,22 @@
+/*
+Question: Inverted Right Angle Star Pattern
+
+Logic:
+- Outer loop decides the row.
+- Inner loop prints stars.
+- Every new row has one less star than the previous row.
+*/
+import java.util.*;
+public class InvertedRightAngleStarPattern{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter n= ");
+        int n=sc.nextInt();
+        for (int i= n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
